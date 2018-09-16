5732c6072f8f214f360b7911c241655001d30402
Elichai Turkel <elichai@enigma.co>
2018-09-16 21:46:06 +0300

    Initializing project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CodeBot',
+    version='',
+    packages=[],
+    url='',
+    license='',
+    author='Elichai',
+    author_email='',
+    description=''
+)
