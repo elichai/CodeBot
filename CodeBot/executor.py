062185de76e60af37140584aa6a0863aaa61ebd2
Isan-Rivkin <isanrivkin@gmail.com>
2018-09-16 22:52:01 +0300

    Added Executor class

--- CodeBot/executor.py
+++ CodeBot/executor.py
@@ -1,9 +1,32 @@
 import docker
-client = docker.from_env()
-#result = client.containers.run("continuumio/anaconda3", "echo hello world")
-code = "print('running code')"
-loop = "for i in range(0,10):\n\tprint('hello')"
-loop2 = "for i in range(0,10):print('my name is {}'.format(i));"
-cmd = 'python -c """{0}"""'
-result = client.containers.run("continuumio/anaconda3", cmd.format(loop2), True,True)
-print(result)
+
+class Executor(object):
+    
+    def __init__(self,container_name):
+    
+        self.client = docker.from_env()
+        self.container_name = container_name
+
+    def validate_code(self,code):
+    
+        return True 
+
+    def execute(self,code):
+    
+        cmd = 'python -c """{0}"""'
+        result = self.client.containers.run(self.container_name, cmd.format(code), True,True)
+        return result
+
+    def format_bytes(self,unformatted_result):
+        return unformatted_result
+        
+#loop2 = "for i in range(0,10):print('my name is {}'.format(i));"
+def example(code):
+    executor = Executor("continuumio/anaconda3")
+    result = executor.execute(code)
+    pretty_result = executor.format_bytes(result)
+    print(pretty_result)
+
+
+# loop = "for i in range(0,10):print('my name is {}'.format(i));"
+# example(loop)
