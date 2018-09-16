3253514257a655aa47d3e3e9381af30afa037974
Isan-Rivkin <isanrivkin@gmail.com>
2018-09-16 23:17:03 +0300

    executor errors

--- CodeBot/executor.py
+++ CodeBot/executor.py
@@ -12,13 +12,18 @@ class Executor(object):
         return True 
 
     def execute(self,code):
-    
         cmd = 'python -c """{0}"""'
-        result = self.client.containers.run(self.container_name, cmd.format(code), True,True)
-        return result
+        try:
+            result = self.client.containers.run(self.container_name, cmd.format(code), True,True)
+            return result
+        except Exception as e:
+            return self.format_error(e)
 
     def format_bytes(self,unformatted_result):
         return unformatted_result
+
+    def format_error(self,unformatted_err):
+            output = str(unformatted_err)[-140:]
         
 #loop2 = "for i in range(0,10):print('my name is {}'.format(i));"
 def example(code):
@@ -28,5 +33,7 @@ def example(code):
     print(pretty_result)
 
 
-# loop = "for i in range(0,10):print('my name is {}'.format(i));"
-# example(loop)
+if __name__ == '__main__':
+    loop = "for i in range(0,10):print('my name is {}'.format(i));"
+    bad_code = "for i in range(0,10):prinsdt('my name is {}'.format(i));"
+    example(loop)
